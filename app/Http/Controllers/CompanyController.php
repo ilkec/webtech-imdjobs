2298a3de6fe66ca3cfa7ec22426952a3c351195a
tikoStudio <timothy.koenig@mail.com>
2020-10-16 14:19:23 +0200

    hardcoded tester url for company completion

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Classes\Foursquare;
 
 class CompanyController extends Controller
 {
@@ -25,6 +26,11 @@ class CompanyController extends Controller
     public function updateCompany($id)
     {
         $data['company'] =  \App\Models\Companies::where('id', $id)->first();
-        return view('/company/update', $data);
+        $url = "https://api.foursquare.com/v2/venues/search?near=Brussels&query=HUNTRS&client_id=4TAFZM0IL2S430ZFFPTO5ILZRM1GLRD2QRELEPDEYIADKF5V&client_secret=1W1UAU1GEYO2E3BA5Q45BT1FNAXNM5P5ZP2JJZ3CBAUCDMBB&v=20180323";
+        $json = file_get_contents($url);
+        $elements = json_decode($json);
+        var_dump($elements);
+        //$data['foursquare'] =
+        //return view('/company/update', $data);
     }
 }
