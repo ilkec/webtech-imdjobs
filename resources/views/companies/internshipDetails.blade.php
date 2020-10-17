a569b53dd745a70e181c3448766bae0a700b4edc
Yanis705 <yanis705@icloud.com>
2020-10-17 15:42:38 +0200

    Output usernames instead of id's

--- resources/views/companies/internshipDetails.blade.php
+++ resources/views/companies/internshipDetails.blade.php
@@ -1,3 +1,4 @@
+@include('partials.header')
 <h1>Internship details</h1>
 @foreach( $details as $detail)
     <p>{{ $detail->title }}</p>
@@ -5,10 +6,37 @@
     <p>{{ $detail->tasks }}</p>
 @endforeach
 <h1>Applications for this internship</h1>
-@foreach( $applications as $application)
-    <p>{{ $application->user_id }}</p>
-@endforeach
-
 @foreach( $users as $user)
-    <p>{{ $user }}</p>
+    <p>{{ $user[0]->first_name }}</p>
 @endforeach
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+@include('partials.footer')
