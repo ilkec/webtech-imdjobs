1d3264978140159574d9697127db06eefd16b424
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-10-19 20:56:10 +0200

    user migration: added cv - public/images: added avartar - userprofile: images crop not ready

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,10 +8,15 @@
         <div class="alert alert-success">{{ $flash }}</div>
     @endif
     <h1>Hi, i'm {{ $users->first_name }} {{ $users->last_name }}</h1>
-    <div class="img-thumbnail">
-        <img src="{{ asset('storage/' . $users->picture) }}" alt="profilepicture" id="profilePicture">
+    @if (empty( $users->picture ))
+    <div class="container-img">
+        <img class="img-thumbnail" src="{{ asset('images/profilePic.jpg') }}" alt="profilepicture" id="profilePicture">
     </div>
-    
+    @else
+    <div class="container-img">
+        <img class="img-thumbnail" src="{{ asset('storage/' . $users->picture) }}" alt="profilepicture" id="profilePicture">
+    </div>
+    @endif
     
     <div class='description'>
         <h4>What I would like you to know</h4>
@@ -24,6 +29,13 @@
         <p>{{ $users->phone_number }}</p>
     </div>
     
+    @if (!empty( $users->cv ))
+    <a href="{{ asset('storage/' . $users->cv) }}" download="cv_{{ $users->first_name }}_{{ $users->last_name }}">
+        <p>Download my cv</p>
+    </a>    
+    @endif
+    
+    @if (!empty( $users->website ) || !empty( $users->linkedin ) || !empty( $users->dribbble ) || !empty( $users->behance ) || !empty( $users->github ) )
     <div class='links'>
         <h4>Glimpse of my portfolio </h4>
         @if (!empty( $users->website ))
@@ -38,6 +50,10 @@
         @if (!empty( $users->behance ))
             <a href="{{ $users->behance }}"><p>Behance</p></a>
         @endif
+        @if (!empty( $users->github ))
+            <a href="{{ $users->github }}"><p>Github</p></a>
+        @endif
+    @endif
         
         @if( $flash = session('User') === $users->id )
             <a href="/user/update"><button type="button" class="btn btn-primary">update profile</button></a>
