bbeb793a9e44f55c265589c10dae855252170d37
tikoStudio <timothy.koenig@mail.com>
2020-10-25 11:11:45 +0100

    input field in update company stay filled in after failed validation

--- resources/views/company/update.blade.php
+++ resources/views/company/update.blade.php
@@ -34,20 +34,20 @@
 
         <div class="form-group">
             <label for="province">Province</label>
-            <input name="province" type="text" class="form-control" id="province" value="@if(!empty($foursquare['location']['state'])) {{$foursquare['location']['state']}} @endif" placeholder="Province">
+            <input name="province" type="text" class="form-control" id="province" value="@if(!empty(old('province'))) {{old('province')}}  @elseif(!empty($foursquare['location']['state'])) {{$foursquare['location']['state']}} @endif" placeholder="Province">
         </div>
         <div class="form-group">
             <label for="street_address">Street address</label>
-            <input name="street_address" type="text" class="form-control" id="street_address" value="@if(!empty($foursquare['location']['address'])) {{$foursquare['location']['address']}} @endif" placeholder="Street name">
+            <input name="street_address" type="text" class="form-control" id="street_address" value="@if(!empty(old('street_address'))) {{old('street_address')}} @elseif(!empty($foursquare['location']['address'])) {{$foursquare['location']['address']}} @endif" placeholder="Street name">
         </div>
         <div class="form-group">
             <label for="postal_code">Postal code</label>
-            <input name="postal_code" type="text" class="form-control" id="postal_code" value="@if(!empty($foursquare['location']['postalCode'])) {{$foursquare['location']['postalCode']}} @endif" placeholder="Postal Code">
+            <input name="postal_code" type="text" class="form-control" id="postal_code" value="@if(!empty(old('postal_code'))) {{old('postal_code')}} @elseif(!empty($foursquare['location']['postalCode'])) {{$foursquare['location']['postalCode']}} @endif" placeholder="Postal Code">
         </div>
 
         <div class="form-group">
             <label for="description">description</label>
-            <textarea name="description" class="form-control" id="description">@if(!empty($foursquare['categories'][0]['name'])) {{$foursquare['categories'][0]['name']}} @endif</textarea>
+            <textarea name="description" class="form-control" id="description">@if(!empty(old('description'))) {{old('description')}} @elseif(!empty($foursquare['categories'][0]['name'])) {{$foursquare['categories'][0]['name']}} @endif</textarea>
         </div>
         <div class="form_group">
             <input type="file" name="image" class="btn mb-3" id="image" accept="image/*">
@@ -55,11 +55,11 @@
         </div>
         <div class="form-group">
             <label for="email">email</label>
-            <input name="email" type="email" class="form-control" id="email" placeholder="Company email">
+            <input name="email" type="email" class="form-control" id="email" value="@if(!empty(old('email'))) {{old('email')}} @endif" placeholder="Company email">
         </div>
         <div class="form-group">
             <label for="phone_number">Phone number</label>
-            <input name="phone_number" type="tel" class="form-control" id="phone_number" placeholder="Company phone number">
+            <input name="phone_number" type="tel" class="form-control" id="phone_number" value="@if(!empty(old('phone_number'))) {{old('phone_number')}} @endif" placeholder="Company phone number">
         </div>
         
         <button type="submit" class="btn btn-primary"> Update company</button>
