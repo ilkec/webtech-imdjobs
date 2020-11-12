57a869a5aaac82db5ded5b8b974aec1accb4c76e
tikoStudio <timothy.koenig@mail.com>
2020-11-12 11:22:57 +0100

    company list page layout

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -6,15 +6,17 @@
 
 @section('content')
     <h1>All companies</h1>
-    @foreach( $companies as $company)
-    <div>
-        <a href="/companies/{{ $company->id }}"><h2>{{ $company->name }}</h2></a>
-        <div class='description'>
-            <p>{{$company->description}}</p>
+    <section class="list list--companies">
+        @foreach( $companies as $company)
+        <div class="list__item">
+            <a href="/companies/{{ $company->id }}"><h2>{{ $company->name }}</h2></a>
+            <div class='description'>
+                <p>{{$company->description}}</p>
+            </div>
+            <div class="container-img">
+                <img class="img-thumbnail" src="{{ asset('storage/' . $company->picture) }}" width="500" height="500" alt="companylogo" id="companyPicture">
+            </div>
         </div>
-        <div class="container-img">
-            <img class="img-thumbnail" src="{{ asset('storage/' . $company->picture) }}" width="500" height="500" alt="companylogo" id="companyPicture">
-        </div>
-    </div>
-    @endforeach
+        @endforeach
+    </section>
 @endsection
