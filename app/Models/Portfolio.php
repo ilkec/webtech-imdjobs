c92c3cba8f63dcf48180d7a02ff924a0fdf13623
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-11-17 15:50:50 +0100

    made new migration portfolio - new model portfolio

---
+++ app/Models/Portfolio.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Portfolio extends Model
+{
+    use HasFactory;
+    protected $table = 'portfolio'; //defines which table to store data to - normally a portfolio model will store in portfolios table
+   
+    public function user(){
+        return $this->belongsTo('\App\Models\User');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+
+    public function portfolio(){
+        return $this->hasMany('\App\Models\Portfolio');
+    }
     /**
      * The attributes that are mass assignable.
      *
