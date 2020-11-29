d8dc88752bdc7984cce23f38f663be2b4a144836
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-11-29 20:47:39 +0100

    edit Envoy file -> typo servers

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,21 +1,21 @@
-@server( ['production' => 'interact.crabdance.com@172.104.151.134', 'staging' => 'interact.crabdance.com@172.104.151.134'])
+@servers(['production' => 'interact.crabdance.com@172.104.151.134', 'staging' => 'interact.crabdance.com@172.104.151.134'])
 
 @task('deploy-production', ['on' => 'production'])
-cd /home/interact.crabdance.com/production/webtech-imdjobs
-php artisan down
-git reset --hard HEAD 
-git pull origin master 
-php composer.phar dump-autoload
-php artisan migrate --force 
-php artisan up
+    cd /home/interact.crabdance.com/production/webtech-imdjobs
+    php artisan down
+    git reset --hard HEAD 
+    git pull origin master 
+    php composer.phar dump-autoload
+    php artisan migrate --force 
+    php artisan up
 @endtask
 
 @task('deploy-staging', ['on' => 'staging'])
-cd /home/interact.crabdance.com/staging/webtech-imdjobs
-php artisan down
-git reset --hard HEAD 
-git pull origin master 
-php composer.phar dump-autoload
-php artisan migrate --force 
-php artisan up
+    cd /home/interact.crabdance.com/staging/webtech-imdjobs
+    php artisan down
+    git reset --hard HEAD 
+    git pull origin master 
+    php composer.phar dump-autoload
+    php artisan migrate --force 
+    php artisan up
 @endtask
