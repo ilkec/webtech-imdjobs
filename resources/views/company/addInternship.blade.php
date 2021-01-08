d76b163d2152fcbe2eb5220d62db73a851c4914b
tikoStudio <timothy.koenig@mail.com>
2021-01-08 16:25:46 +0100

    bugfix: add internship form forgot to add old value on error validation

--- resources/views/company/addInternship.blade.php
+++ resources/views/company/addInternship.blade.php
@@ -22,19 +22,19 @@
 
         <div class="form-group">
             <label for="title">internship title *</label>
-            <input name="title" type="text" class="form-control" id="title" placeholder="Internship title">
+            <input name="title" type="text" class="form-control" id="title" placeholder="Internship title" value="{{old('title')}}">
         </div>
         <div class="form-group">
             <label for="description">description *</label>
-            <textarea name="description" class="form-control" id="description" placeholder="Our company values its employees and has a real passion for:"></textarea>
+            <textarea name="description" class="form-control" id="description" placeholder="Our company values its employees and has a real passion for:">{{old('description')}}</textarea>
         </div>
         <div class="form-group">
             <label for="tasks">tasks *</label>
-            <textarea name="tasks" class="form-control" id="tasks" placeholder="the tasks you can expect at this internship are:"></textarea>
+            <textarea name="tasks" class="form-control" id="tasks" placeholder="the tasks you can expect at this internship are:">{{old('tasks')}}</textarea>
         </div>
         <div class="form-group">
             <label for="profile">profile *</label>
-            <textarea name="profile" class="form-control" id="profile" placeholder="the profile we are looking for is:"></textarea>
+            <textarea name="profile" class="form-control" id="profile" placeholder="the profile we are looking for is:">{{old('profile')}}</textarea>
         </div>
         <div class="form-group">
             <label for="type">Internship type *</label>
@@ -49,11 +49,11 @@
 
         <div class="form-group">
             <label for="city">City *</label>
-            <input name="city" type="text" class="form-control" id="city" value="@if(!empty($company->city)) {{$company->city}} @endif" placeholder="City">
+            <input name="city" type="text" class="form-control" id="city" value="@if(!empty($company->city)) {{$company->city}}@else {{old('city')}} @endif" placeholder="City">
         </div>
         <div class="form-group">
             <label for="postal_code">Postal code *</label>
-            <input name="postal_code" type="text" class="form-control" id="postal_code" value="@if(!empty($company->postal_code)) {{$company->postal_code}} @endif" placeholder="Postal Code">
+            <input name="postal_code" type="text" class="form-control" id="postal_code" value="@if(!empty($company->postal_code)) {{$company->postal_code}} @else {{old('postal_code')}} @endif" placeholder="Postal Code">
         </div>
         
         <button type="submit" class="btn btn-primary">Add internship</button>
